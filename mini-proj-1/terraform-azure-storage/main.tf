400a902294c2a7f8ac94ac4cfb1398b4bf614793
jkgaurav <50937472+jkgaurav@users.noreply.github.com>
2024-09-18 10:34:27 +0530

    Create main.tf

---
+++ mini-proj-1/terraform-azure-storage/main.tf
@@ -0,0 +1,25 @@
+# Provider configuration for Azure
+provider "azurerm" {
+  features {}
+
+  # Specifies the Azure subscription where Terraform will create resources.
+  subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+  # Prevents automatic registration of new Azure resource providers.
+  resource_provider_registrations = "none"
+}
+
+# Create a new Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create a new Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
