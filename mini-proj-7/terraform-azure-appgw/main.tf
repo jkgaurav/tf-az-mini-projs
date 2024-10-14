26e3aba93f816a98cfd57ee9c8b603eaca8216de
jkgaurav <50937472+jkgaurav@users.noreply.github.com>
2024-10-14 17:51:58 +0530

    Update main.tf

--- mini-proj-7/terraform-azure-appgw/main.tf
+++ mini-proj-7/terraform-azure-appgw/main.tf
@@ -1,1 +1,10 @@
+# Provider configuration for Azure
+provider "azurerm" {
+  features {}
 
+  # Specifies the Azure subscription where Terraform will create resources.
+  subscription_id = "4788db51-f5bb-442d-8cdd-a6debf476da9"
+
+  # Prevents automatic registration of new Azure resource providers.
+  resource_provider_registrations = "none"
+}
