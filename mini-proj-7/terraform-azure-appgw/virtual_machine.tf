9c4152a00bf72190d3d76d05c7ee29198a8f422a
jkgaurav <50937472+jkgaurav@users.noreply.github.com>
2024-10-14 17:54:00 +0530

    Create virtual_machine.tf

---
+++ mini-proj-7/terraform-azure-appgw/virtual_machine.tf
@@ -0,0 +1,55 @@
+# Create a Network Interface for the VM
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Virtual Machine (Backend)
+resource "azurerm_linux_virtual_machine" "backend_vm" {
+  name                = "backend-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1ms"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Custom Script Extension to Install nginx
+resource "azurerm_virtual_machine_extension" "install_nginx" {
+  name                 = "install-nginx"
+  virtual_machine_id   = azurerm_linux_virtual_machine.backend_vm.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "sudo apt update && sudo apt install -y nginx && sudo systemctl start nginx"
+    }
+  SETTINGS
+}
